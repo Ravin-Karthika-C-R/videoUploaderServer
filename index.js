708e7ef70f4d4020b82a931c92c59b9cffa80703
ravin <ravinkarthikacr1998@gmail.com>
2023-11-20 13:53:06 +0530

    new

---
+++ index.js
@@ -0,0 +1,20 @@
+//import json server
+const jsonServer=require('json-server')
+const cors=require('cors')
+
+const server=jsonServer.create()
+
+//middlewares to convert json to js
+const middleware=jsonServer.defaults()
+const router=jsonServer.router('data.json')
+
+server.use(cors())
+server.use(middleware)
+server.use(router)
+
+const PORT=4000
+    server.listen(PORT,()=>{
+        console.log(`...Json Server Started At Port ${PORT} ...`);
+    })
+
+    
